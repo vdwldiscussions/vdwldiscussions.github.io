0ae8bf3304d88b17dec47b0919f13ac92c16ed9a
suadou <92791932+suadou@users.noreply.github.com>
2025-01-07 20:04:28 +0100

    Add files via upload

---
+++ contactform/contactform.js
@@ -0,0 +1,120 @@
+jQuery(document).ready(function($) {
+  "use strict";
+
+  //Contact
+  $('form.contactForm').submit(function() {
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+    f.children('input').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'checked':
+            if (!i.attr('checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    // Prepare data for Google Form submission
+    
+    var formData = $(this).serialize();
+    // console.log(formData)
+    var googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSfRP2ZGTlYwYtYvwrhwPtSTzNaDaV8yaAA8AZFmAoJAWsqw2A/formResponse?"; // Replace with your Google Form URL
+    const container = document.getElementById("contactContainer");
+    const iframe = document.createElement("iframe");
+    iframe.src = googleFormURL+formData + "&susubmit=Submit";
+    iframe.name = "contact-iframe";
+    iframe.id = "contact-iframe";
+    iframe.style = "display:none;";
+    container.innerHTML = "";
+    container.appendChild(iframe);
+
+
+    var response = document.getElementById('contact-iframe');
+    if (response) {
+      $("#sendmessage").addClass("show");
+      $("#errormessage").removeClass("show");
+      $('.contactForm').find("input, textarea").val("");
+    } else {
+      $("#sendmessage").removeClass("show");
+      $("#errormessage").addClass("show");
+      $('#errormessage').html("There was an error submitting the form. Please try again.");
+    }
+
+    return false;
+  });
+
+});
---
+++ registrationform/registrationform.js
@@ -0,0 +1,123 @@
+jQuery(document).ready(function($) {
+  "use strict";
+
+  //Contact
+  $('form.registrationForm').submit(function() {
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+    f.children('input').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'checked':
+            if (!i.attr('checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    // Prepare data for Google Form submission
+    
+    var formData = $(this).serialize();
+    // console.log(formData)
+    var googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSeheh5-uwlpCq7PW86eQSB4IMkiW3ruHQbW0uBDa8SMd7CDzg/formResponse?"; // Replace with your Google Form URL
+    const container = document.getElementById("registerContainer");
+    const iframe = document.createElement("iframe");
+    iframe.src = googleFormURL+formData + "&susubmit=Submit";
+    iframe.name = "register-iframe";
+    iframe.id = "register-iframe";
+    iframe.style = "display:none;";
+    container.innerHTML = "";
+    container.appendChild(iframe);
+
+
+    var response = document.getElementById('register-iframe');
+    if (response) {
+      $("#sendregistration").addClass("show");
+      $("#errorregistration").removeClass("show");
+      $('.registrationForm').remove()
+    } else {
+      $("#sendregistration").removeClass("show");
+      $("#errorregistration").addClass("show");
+      $('#errorregistration').html("There was an error submitting the form. Please try again.");
+    }
+
+
+
+
+    return false;
+  });
+
+});
